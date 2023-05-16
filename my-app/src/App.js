b26c027f1b2d2e36dd922d273d5fd871eaf0ec69
nick200215 <96425571+nick200215@users.noreply.github.com>
2023-05-16 12:46:34 +0400

    my first commite

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+// import logo from "./logo.svg";
+// import "./App.css";
+import "./assets/style/style.css";
+import "./assets/img";
+// import { Header } from "./components/header.jsx";
+import { Header } from "./components/header.jsx";
+
+function App() {
+  return (
+    <>
+      <header />
+    </>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/header.jsx
@@ -0,0 +1,53 @@
+// import React from "react";
+
+// const Header = () => {
+//   return (
+//     <header>
+//       <div className="wrapper header-wrapp">
+//         <figure>
+//           <a href="./index.html">
+//             <img className="logo" src="./assets/img/1.png" alt="" />
+//           </a>
+//         </figure>
+//         <div className="nav-button">
+//           <nav>
+//             <ul>
+//               <li>
+//                 <a href="./index.html">Home</a>
+//               </li>
+//               <li>
+//                 <a href="./index.html">Demo</a>
+//               </li>
+//               <li>
+//                 <a href="./index.html">Inner Pages</a>
+//               </li>
+//               <li>
+//                 <a href="./index.html">Support</a>
+//               </li>
+//               <li>
+//                 <a href="./index.html">Feature</a>
+//               </li>
+//               <li>
+//                 <a href="./index.html">Blog</a>
+//               </li>
+//             </ul>
+//           </nav>
+//           <button>
+//             <a href="./index.html">Get Started</a>
+//           </button>
+//         </div>
+//       </div>
+//     </header>
+//   );
+// };
+
+// // export  Header;
+// export { Header };
+
+import React from "react";
+
+const Header = () => {
+  return <header>{/* Header JSX content */}</header>;
+};
+
+export { Header };
