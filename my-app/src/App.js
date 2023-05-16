c51d0f9137b4c54e5092a53267c8e7504fd2519b
nick200215 <96425571+nick200215@users.noreply.github.com>
2023-05-16 13:11:53 +0400

    next commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,10 @@
-// import logo from "./logo.svg";
-// import "./App.css";
 import "./assets/style/style.css";
-import "./assets/img";
-// import { Header } from "./components/header.jsx";
 import { Header } from "./components/header.jsx";
 
 function App() {
   return (
     <>
-      <header />
+      <Header />
     </>
   );
 }
--- my-app/src/components/header.jsx
+++ my-app/src/components/header.jsx
@@ -1,53 +1,45 @@
-// import React from "react";
-
-// const Header = () => {
-//   return (
-//     <header>
-//       <div className="wrapper header-wrapp">
-//         <figure>
-//           <a href="./index.html">
-//             <img className="logo" src="./assets/img/1.png" alt="" />
-//           </a>
-//         </figure>
-//         <div className="nav-button">
-//           <nav>
-//             <ul>
-//               <li>
-//                 <a href="./index.html">Home</a>
-//               </li>
-//               <li>
-//                 <a href="./index.html">Demo</a>
-//               </li>
-//               <li>
-//                 <a href="./index.html">Inner Pages</a>
-//               </li>
-//               <li>
-//                 <a href="./index.html">Support</a>
-//               </li>
-//               <li>
-//                 <a href="./index.html">Feature</a>
-//               </li>
-//               <li>
-//                 <a href="./index.html">Blog</a>
-//               </li>
-//             </ul>
-//           </nav>
-//           <button>
-//             <a href="./index.html">Get Started</a>
-//           </button>
-//         </div>
-//       </div>
-//     </header>
-//   );
-// };
-
-// // export  Header;
-// export { Header };
-
 import React from "react";
+import logoImage from "./../assets/img/1.png";
 
 const Header = () => {
-  return <header>{/* Header JSX content */}</header>;
+  return (
+    <header>
+      <div className="wrapper header-wrapp">
+        <figure>
+          <a href="./index.html">
+            <img className="logo" src={logoImage} alt="" />
+          </a>
+        </figure>
+        <div className="nav-button">
+          <nav>
+            <ul>
+              <li>
+                <a href="./index.html">Home</a>
+              </li>
+              <li>
+                <a href="./index.html">Demo</a>
+              </li>
+              <li>
+                <a href="./index.html">Inner Pages</a>
+              </li>
+              <li>
+                <a href="./index.html">Support</a>
+              </li>
+              <li>
+                <a href="./index.html">Feature</a>
+              </li>
+              <li>
+                <a href="./index.html">Blog</a>
+              </li>
+            </ul>
+          </nav>
+          <button>
+            <a href="./index.html">Get Started</a>
+          </button>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export { Header };
